e9621f99cc33e56f120a1cd3bd41e32c7c89edcb
poojakrp777 <poojakrp777@gmail.com>
2022-11-02 19:18:00 +0530

    group by gender and calculate avg sum min max sal

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,6 +10,8 @@ Create Table EmployeePayroll(Id int identity(1,1)primary key, Empname varchar(20
 --UC3 insert record--
 insert into EmployeePayroll(Empname,Salary,Startdate) values('Bill',100000,'2018-09-07');
 insert into EmployeePayroll(Empname,Salary,Startdate) values('charlie',200000,'2019-07-04');
+insert into EmployeePayroll(Empname,Salary,Startdate) values('Pooja',300000,'2018-08-06');
+insert into EmployeePayroll(Empname,Salary,Startdate) values('Abhi',250000,'2019-03-01');
 
 --UC4 Retrieve the data--
 select * from EmployeePayroll;
@@ -21,5 +23,14 @@ select * from EmployeePayroll WHERE Startdate BETWEEN CAST('2019-02-13' AS DATE)
 alter table EmployeePayroll add Gender char(1)
 update EmployeePayroll set Gender = 'M' where Empname='Bill'
 update EmployeePayroll set Gender = 'M' where Empname='charlie'
-
+update EmployeePayroll set Gender = 'M' where Empname='Abhi'
+update EmployeePayroll set Gender = 'F' where Empname='Pooja'
 select * from EmployeePayroll;
+
+--UC7-Avg, sum, max, min salary
+select Sum(Salary) from EmployeePayroll where Gender='M' group by Gender;
+	select Sum(Salary) as Total_Sum from EmployeePayroll where Gender='M' group by Gender;
+	select Avg(Salary) as Total_Avg from EmployeePayroll where Gender='M' group by Gender;
+	select Min(Salary) as Minsalary from EmployeePayroll where Gender='M' group by Gender;
+	select Max(Salary) as Maxsalary from EmployeePayroll where Gender='M' group by Gender;
+	select COUNT (*)TotalRecord from EmployeePayroll;

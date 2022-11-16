51ebcaba9f0ad2da9214271a47ef78550c54d059
poojakrp777 <poojakrp777@gmail.com>
2022-11-16 13:28:33 +0530

    ER diagram of mssql

--- Addressbookquery.sql
+++ Addressbookquery.sql
@@ -42,5 +42,55 @@ select * from AddressBook
 update AddressBook set Type='Friend' where FirstName='Abhi'
 update AddressBook set Type='Family' where FirstName='Ankitha'
 update AddressBook set Type='Profession' where FirstName='Basava'
-update AddressBook set Type='Friend' where FirstName='Pavitra'
+update AddressBook set Type='Friend' where FirstName='Pooja'
 select * from AddressBook
+
+--UC 10 count by type
+select count (Type) from AddressBook where Type= 'Family'
+select count (Type) from AddressBook where Type= 'Profession'
+select count (Type) from AddressBook where Type= 'Friend'
+
+--UC 11 add person to both Friend and Family
+insert into AddressBook values ('kavi', 'pkr', 'bng', 'lgr', 'kar', 573, 95893478962, 'kavi@gmail.com','Friend And Family' )
+select * from AddressBook
+
+--UC12 - Creating different entities.
+--creating table of typeofcontacts
+create table TypesOfContacts
+(typeid int primary key ,typename varchar(50) not null);
+
+--inserting data into type of contacts
+insert into Typesofcontacts values(1,'Family'),(2,'Friends'),(3,'Business');
+
+alter table AddressBook
+add contactid int primary key identity(1,1);
+select * from AddressBook;
+
+--Creating table of address book names
+create table AddressBookNames(addressBookId int primary key identity(1,1),addressBookName varchar(50) not null );
+--inserting values to addressbook table
+insert into AddressBooknames values ('P'),('A'),('B');
+select * from AddressBookNames;
+
+--creating table address book names mapper which will contain contact id and address book names id
+create table AddressbookMapping(contactid int not null, addressbookid int not null);
+--inserting data into address book mapper id
+insert into AddressbookMapping values (1,1),(2,1),(3,2);
+select * from AddressbookMapping;
+
+select a.firstname, a.phoneNumber, a.city, a.state, a.eMail, b.addressbookname, b.addressBookId
+from AddressBook a
+join AddressbookMapping d
+on a.contactid = d.contactId
+join AddressBookNames b
+on b.addressBookId= d.addressbookId
+
+--UC13 - Retrieving data using new table structure.
+--For UC6
+select FirstName,LastName,city from AddressBook where FirstName='Pooja';
+
+--For UC7
+select city,count(*) from AddressBook where city='BNG' group by city;
+
+--For UC8
+select * from  AddressBook where city='ckm' order by FirstName,LastName;
